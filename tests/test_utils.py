176aea317ba33e74636e2568aa96c5ad7b0073e6
Kate Wang <yanzhe_wang@163.com>
2021-05-28 19:23:48 +0800

    add asyncio version and some small tweaks for prior version

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,24 +3,37 @@ import sys
 import os
 import json
 from unittest import mock
+import aiohttp
+
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-from fake_useragent import utils, errors, settings
+from fake_useragent import aio_utils, utils, errors, settings
 
 # NOTE Google blocks requests not from browser, don't test with google url
-def test_utils_get():
-    assert utils.get("https://i3wm.org") is not None
-    assert utils.get("http://info.cern.ch/") is not None
+def test_utils_fetch():
+    assert utils.fetch("https://i3wm.org") is not None
+    assert utils.fetch("http://info.cern.ch/") is not None
 
     # This wouldn't take effect, because requests library verify certificate expired or not
     # whereas urllib doesn't verify expiration, so this line is only for urllib
     # with pytest.raises(errors.FakeUserAgentError):
     #    utils.get("https://expired.badssl.com/")
 
-    with pytest.raises(errors.FakeUserAgentError):
-        utils.get(settings.BROWSERS_STATS_PAGE)
+
+@pytest.mark.asyncio
+async def test_aio_utils_fetch():
+    async with aiohttp.ClientSession() as session:
+        assert aio_utils.fetch("https://i3wm.org", session) is not None
+        assert aio_utils.fetch("http://info.cern.ch/", session) is not None
+        with pytest.raises(errors.FakeUserAgentError):
+            aio_utils.fetch(settings.BROWSERS_STATS_PAGE, session)
+
+
+@pytest.mark.asyncio
+async def test_aio_utils_load():
+    assert aio_utils.load() is not None
 
 
 # After get rid of get_browsers(), this way of test doesn't needed
@@ -48,7 +61,7 @@ def test_utils_get():
 
 
 def test_utils_load():
-    data = utils.load(use_cache_server=True)
+    data = utils.load()
     x = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
 
     assert data is not None
@@ -58,7 +71,7 @@ def test_utils_load():
 
 
 def test_utils_get_cache_server():
-    body = utils.get(settings.CACHE_SERVER)
+    body = utils.fetch(settings.CACHE_SERVER)
 
     data = json.loads(body)
 
