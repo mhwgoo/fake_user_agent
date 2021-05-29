9ccec8bb18774cef0135cdda48b540e509d8a618
Kate Wang <yanzhe_wang@163.com>
2021-05-29 09:46:53 +0800

    did some refactoring for aio version

---
+++ fake_useragent/aio_main.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import json
+import random
+from time import sleep
+from urllib.parse import quote_plus
+from collections import defaultdict
+import asyncio
+import aiohttp
+from aiohttp import ClientSession
+from lxml import etree
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from fake_useragent import settings
+from fake_useragent.log import logger
+from fake_useragent.errors import FakeUserAgentError
+
+
+all_versions = defaultdict(list)
+
+
+async def fetch(url, session):
+    attempt = 0
+    while True:
+        async with session.get(
+            url, timeout=settings.HTTP_TIMEOUT, verify_ssl=False
+        ) as resp:
+            # try/except block should be inside async with context manager, or it'll break
+            try:
+                attempt += 1
+                result = await resp.text()
+            except aiohttp.ServerTimeoutError:
+                logger.error("Error occurred during fetching %s", url)
+                if attempt == settings.HTTP_RETRIES:
+                    raise FakeUserAgentError("Maximum amount of retries reached")
+                else:
+                    logger.info("Sleeping for %s seconds", settings.HTTP_DELAY)
+                    sleep(settings.HTTP_DELAY)
+            else:
+                return result
+
+
+async def parse(browser, session):
+    html_str = await fetch(
+        settings.BROWSER_BASE_PAGE.format(browser=quote_plus(browser)), session
+    )
+    lxml_element = etree.HTML(html_str)
+    versions = lxml_element.xpath('//*[@id="liste"]/ul/li/a/text()')[
+        : settings.BROWSERS_COUNT_LIMIT
+    ]
+    all_versions[browser].extend(versions)
+
+
+def write(path, data):
+    with open(path, encoding="utf-8", mode="wt") as f:
+        dumped = json.dumps(data)
+        f.write(dumped)
+
+
+def read(path):
+    with open(path, encoding="utf-8", mode="rt") as f:
+        cache_data = f.read()
+        return json.loads(cache_data)
+
+
+def random_choose(browser):
+    data = read(settings.DB)
+    if browser:
+        print(random.choice(data[browser]))
+
+    else:
+        browser = random.choices(
+            list(settings.BROWSERS.keys()),
+            weights=list(settings.BROWSERS.values()),
+            k=1,
+        )[0]
+        print(random.choice(data[browser]))
+
+
+async def main(browser):
+    if os.path.isfile(settings.DB):
+        if browser:
+            if not isinstance(browser, str):
+                raise FakeUserAgentError("Please input a valid browser name")
+            browser = browser.strip().lower()
+            browser = settings.SHORTCUTS.get(browser, browser)
+            if browser not in list(settings.BROWSERS.keys()):
+                raise FakeUserAgentError("This browser is not supported.")
+        random_choose(browser)
+
+    else:
+        async with ClientSession() as session:
+            tasks = []
+            for BROWSER in settings.BROWSERS.keys():
+                tasks.append(parse(BROWSER, session))
+            await asyncio.gather(*tasks)
+            write(settings.DB, all_versions)
+            random_choose(browser)
+
+
+if __name__ == "__main__":
+    input = input("Input a browser name or hit <enter> not to specify browser: ")
+    asyncio.run(main(browser=input))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,37 +9,32 @@ import aiohttp
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-from fake_useragent import aio_utils, utils, errors, settings
+from fake_useragent import aio_main, main, errors, settings
 
-# NOTE Google blocks requests not from browser, don't test with google url
-def test_utils_fetch():
-    assert utils.fetch("https://i3wm.org") is not None
-    assert utils.fetch("http://info.cern.ch/") is not None
+
+def test_main_fetch():
+    assert main.fetch("https://i3wm.org") is not None
+    assert main.fetch("http://info.cern.ch/") is not None
 
     # This wouldn't take effect, because requests library verify certificate expired or not
     # whereas urllib doesn't verify expiration, so this line is only for urllib
     # with pytest.raises(errors.FakeUserAgentError):
-    #    utils.get("https://expired.badssl.com/")
+    #    main.fetch("https://expired.badssl.com/")
 
 
 @pytest.mark.asyncio
-async def test_aio_utils_fetch():
+async def test_aio_main_fetch():
     async with aiohttp.ClientSession() as session:
-        assert aio_utils.fetch("https://i3wm.org", session) is not None
-        assert aio_utils.fetch("http://info.cern.ch/", session) is not None
+        assert aio_main.fetch("https://i3wm.org", session) is not None
+        assert aio_main.fetch("http://info.cern.ch/", session) is not None
         with pytest.raises(errors.FakeUserAgentError):
-            aio_utils.fetch(settings.BROWSERS_STATS_PAGE, session)
+            aio_main.fetch(settings.BROWSERS_STATS_PAGE, session)
 
 
-@pytest.mark.asyncio
-async def test_aio_utils_load():
-    assert aio_utils.load() is not None
-
-
-# After get rid of get_browsers(), this way of test doesn't needed
-# @mock.patch("fake_useragent.utils.get_browsers")
-# def test_utils_load(mock_get_browsers):
-#    # NOTE:side_effect should have same arguments with target function
+# After removing get_browsers(), this test doesn't needed
+# @mock.patch("fake_useragent.main.get_browsers")
+# def test_main_load(mock_get_browsers):
+#    # side_effect should have same arguments with target function
 #    # becaue it replaces target function in test execution
 #    def side_effect(verify_ssl=True):
 #        return [
@@ -51,7 +46,7 @@ async def test_aio_utils_load():
 #        ]
 #
 #    mock_get_browsers.side_effect = side_effect
-#    data = utils.load()
+#    data = main.load()
 #    x = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
 #
 #    assert data is not None
@@ -60,21 +55,18 @@ async def test_aio_utils_load():
 #    assert x in data["browsers"]["chrome"]
 
 
-def test_utils_load():
-    data = utils.load()
+def test_main_load():
+    data = main.load()
     x = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
-
     assert data is not None
     assert isinstance(data["browsers"], dict)
     assert data["browsers"] is not None
     assert x in data["browsers"]["chrome"]
 
 
-def test_utils_get_cache_server():
-    body = utils.fetch(settings.CACHE_SERVER)
-
+def test_main_get_cache_server():
+    body = main.fetch(settings.CACHE_SERVER)
     data = json.loads(body)
-
     expected = {
         "randomize": mock.ANY,
         "browsers": {
@@ -85,5 +77,4 @@ def test_utils_get_cache_server():
             "internetexplorer": mock.ANY,
         },
     }
-
     assert expected == data
