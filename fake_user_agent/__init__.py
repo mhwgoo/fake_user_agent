0b33d2f460494d07611a183d284009c577b9d1c6
Kate Wang <kate.wang2018@gmail.com>
2022-07-29 14:12:54 +0800

    fixed importing issues after packaging

--- fake_user_agent/__init__.py
+++ fake_user_agent/__init__.py
@@ -1,5 +1,6 @@
 "Randomly generates a useragent for fetching a web page without a browser."
 
+from .main import user_agent, use_tempfile
 from .settings import version
 
 __version__ = version
--- fake_user_agent/settings.py
+++ fake_user_agent/settings.py
@@ -7,7 +7,7 @@ import logging
 
 from . import log
 
-version = "2.0.1" 
+version = "2.0.2" 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
